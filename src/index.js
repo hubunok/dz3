6713c5677d8ab774fa618cec0ad146c2fe9d88d8
hubunok <petr.plotnikov83@gmail.com>
2018-11-04 20:24:29 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+function forEach(arr, callback, thisArg) {
+  var i, length = arr.length;
+  for (i = 0; i < length; i = i + 1) {
+    callback.call(thisArg, arr[i], i, arr);
+  }
+};
+
+function map(array, fn) {
+    var copy_array = [];
+    for (var i = 0; i < array.length; i++) {
+        copy_array[i] = fn(array[i], i, array);
+    }
+    return copy_array;
+};
+
+function reduce(array, fn, initial) {
+    var x = initial || array[0],
+        i = initial ? 0 : 1;
+
+    for (; i < array.length; i++) {
+        x = fn(x, array[i], i, array);
+    }
+
+    return x;
+};
+  function upperProps(obj) {
+      var arr = [];
+      for (var key in obj) {
+          key = key.toUpperCase();
+          arr.push(key);
+      }
+      return arr;
+  };
+
+
+export {
+    forEach,
+    map,
+    upperProps,
+    reduce
+};
